6988a9b8c0bbd880d6f3255e41dc1c92c5e926f0
Oleg <pozhenskij2@gmail.com>
2022-01-18 22:51:03 +0400

    integrate bootstrap, dev base.html with Bootstr, dev Category view, add pre-processor

--- store/views.py
+++ store/views.py
@@ -2,6 +2,12 @@ from itertools import product
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
+def categories(request):
+    return {
+        # контекстный процессор, когда данные должены отображаться на всех страницах
+        'categories': Category.objects.all()
+    }
+
 def all_products(request):
     #получение всех объектов
     products = Product.objects.all()
