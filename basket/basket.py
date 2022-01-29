6bf33131a17d806b40b39c622e5af86188a98711
Oleg <pozhenskij2@gmail.com>
2022-01-29 23:23:05 +0400

    add some methods

--- basket/basket.py
+++ basket/basket.py
@@ -27,3 +27,36 @@ class Basket():
             self.basket[product_id] = {'price': str(product.price), 'qty': qty}
 
         self.save()
+
+        
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database
+        and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
+    def update(self, product, qty):
+        """
+        Update values in session data
+        """
+        product_id = str(product)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        self.save()
