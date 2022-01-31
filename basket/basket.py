11ce9db8bcda12dd6434779bcbadad0bcbf631de
Oleg <pozhenskij2@gmail.com>
2022-01-31 23:52:01 +0400

    finish basket class

--- basket/basket.py
+++ basket/basket.py
@@ -75,3 +75,20 @@ class Basket():
         if product_id in self.basket:
             self.basket[product_id]['qty'] = qty
         self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+        """
+        Delete item from session data
+        """
+        product_id = str(product)
+
+        if product_id in self.basket:
+            del self.basket[product_id]
+            print(product_id)
+            self.save()
+
+    def save(self):
+        self.session.modified = True
